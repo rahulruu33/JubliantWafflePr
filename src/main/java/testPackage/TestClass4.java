e16f6bf2065e2106e6afe558621e2142b06b74d8
RU <rahulru_33@outlook.com>
2025-06-14 08:53:15 +0530

    Refactor TestClass4 and TestClass5: update variable types and add test method

--- src/main/java/testPackage/TestClass4.java
+++ src/main/java/testPackage/TestClass4.java
@@ -1,16 +1,15 @@
 package testPackage;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class TestClass4 {
 
 
     public void testMethod(){
+
+        String name2 = "LiFe is Cool";
+        Integer age = 25;
 
 //        List<Employee> employeeList = new ArrayList<>();
 //        employeeList.stream().collect(Collectors.toMap(employee ->employee.getDept(), C))
@@ -23,7 +22,7 @@ public class TestClass4 {
 //        ArrayList<Integer> integerList1  = new ArrayList<>(new HashSet<>(integerList));
 
         String name = "LiFe is Awesome";
-         ArrayList<Integer> integerList = Arrays.asList(1, 2, 4, 5, 6, 2, 4);
+         List<Integer> integerList = Arrays.asList(1, 2, 4, 5, 6, 2, 4);
 
     }
 }
--- src/main/java/testPackage/TestClass5.java
+++ src/main/java/testPackage/TestClass5.java
@@ -4,5 +4,8 @@ import java.util.*;
 
 public class TestClass5 {
 
-
+    public void testMethod5() {
+        // This is a test method in TestClass5
+        System.out.println("TestClass5: testMethod5 executed");
+    }
 }
